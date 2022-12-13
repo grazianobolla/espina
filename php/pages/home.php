b7b6932670cea92299f08e03e63d0f338e3b93bf
Graziano Bolla <grazianobolla12@gmail.com>
2022-12-13 12:26:31 -0300

    bigger home logo

--- php/pages/home.php
+++ php/pages/home.php
@@ -1,6 +1,6 @@
 <!-- About me -->
 <div class="text-center mb-3 d-flex flex-column align-items-center">
-    <img src="<?php echo $site->logo(); ?>" class="rounded-circle" style="width:75px;">
+    <img src="<?php echo $site->logo(); ?>" class="rounded-circle" style="width:90px;">
     <p class="text-muted mt-1 mb-0 font-italic" style="max-width: 300px">
         <?php echo $site->footer(); ?>
     </p>
