16b9eab66b9f2dc6c987982d049e437ac483325f
Graziano Bolla <grazianobolla12@gmail.com>
2022-12-16 15:17:14 -0300

    minor fix

--- php/pages/home.php
+++ php/pages/home.php
@@ -1,7 +1,7 @@
 <!-- About me -->
 <div class="text-center mb-3 d-flex flex-column align-items-center">
     <img src="<?php echo $site->logo(); ?>" class="rounded-circle" style="width:90px; height:90px;" alt="Profile picture">
-    <p class="text-muted mt-1 mb-0 font-italic" style="max-width: 300px">
+    <p class="text-darker mt-1 mb-0 font-italic" style="max-width: 300px">
         <?php echo $site->footer(); ?>
     </p>
 </div>
--- php/pages/page.php
+++ php/pages/page.php
@@ -19,14 +19,15 @@
 
 <?php if (!$page->isStatic() && !$url->notFound()): ?>
 <!-- Creation date -->
-<h6 class="text-muted mb-3">
+<h6 class="text-muted">
 	<?php echo $page->date(); ?> ~
 	<?php echo $page->relativeTime(); ?>
 </h6>
 <?php endif ?>
 
 <!-- Full content -->
-<?php echo $page->content(); ?>
-
+<div class="mt-3">
+	<?php echo $page->content(); ?>
+</div>
 <!-- Load Bludit Plugins: Page End -->
 <?php Theme::plugins('pageEnd'); ?>
